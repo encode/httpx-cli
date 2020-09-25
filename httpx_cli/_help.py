92a5d9d7871ba00c6b5dc3a2654d486865df5a51
Tom Christie <tom@tomchristie.com>
2020-09-25 10:23:08 +0100

    Linting

--- httpx_cli/_help.py
+++ httpx_cli/_help.py
@@ -13,50 +13,41 @@ help_table = [
         "-p, --params [cyan]<NAME VALUE> ...",
         "Query parameters to include in the request URL.",
     ),
-    (
-        "-c, --content [cyan]TEXT", "Byte content to include in the request body."
-    ),
-    (
-        "-d, --data [cyan]<NAME VALUE> ...", "Form data to include in the request body."
-    ),
+    ("-c, --content [cyan]TEXT", "Byte content to include in the request body."),
+    ("-d, --data [cyan]<NAME VALUE> ...", "Form data to include in the request body."),
     (
         "-f, --files [cyan]<NAME FILENAME> ...",
-        "Form files to include in the request body."
+        "Form files to include in the request body.",
     ),
     ("-j, --json [cyan]TEXT", "JSON data to include in the request body."),
     (
         "-h, --headers [cyan]<NAME VALUE> ...",
         "Include additional HTTP headers in the request.",
     ),
-    (
-        "--cookies [cyan]<NAME VALUE> ...", "Cookies to include in the request."
-    ),
+    ("--cookies [cyan]<NAME VALUE> ...", "Cookies to include in the request."),
     (
         "-a, --auth [cyan]<USER PASS>",
         "Username and password to include in the request. Specify '-' for the password to use "
         "a password prompt. Note that using --verbose/-v will expose the Authorization "
-        "header, including the password encoding in a trivially reverisible format."
+        "header, including the password encoding in a trivially reverisible format.",
     ),
     (
         "--proxy [cyan]URL",
-        "Send the request via a proxy. Should be the URL giving the proxy address."
+        "Send the request via a proxy. Should be the URL giving the proxy address.",
     ),
     (
         "-t, --timeout [cyan]FLOAT",
         "Timeout value to use for network operations, such as establishing the connection, "
-        "reading some data, etc... [Default: [bold]5.0[/bold]]"
+        "reading some data, etc... [Default: [bold]5.0[/bold]]",
     ),
     ("--no-allow-redirects", "Don't automatically follow redirects."),
     ("--no-verify", "Disable SSL verification."),
-    (
-        "--http2", "Send the request using HTTP/2, if the remote server supports it."
-    ),
-    (
-        "--download", "Save the response content as a file, rather than displaying it."
-    ),
+    ("--http2", "Send the request using HTTP/2, if the remote server supports it."),
+    ("--download", "Save the response content as a file, rather than displaying it."),
     ("-v, --verbose", "Verbose output. Show request as well as response."),
-    ("--help", "Show this message and exit.")
+    ("--help", "Show this message and exit."),
 ]
+
 
 def print_help() -> None:
     console = Console()
